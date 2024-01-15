dc1cf6636383d31caabbcf4b125fe9e309123f6a
GH <130484247+vvzsf@users.noreply.github.com>
2024-01-15 15:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,5 +34,5 @@ def upload():
     return 'File uploaded successfully'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
               
